ce65615d4c1980ba197acdeac325e054dc1c39db
huyIttos2 <48934784+huyIttos2@users.noreply.github.com>
2019-08-14 16:27:58 +0700

    Update Student.php

--- class/Student.php
+++ class/Student.php
@@ -1,13 +1,11 @@
 <?php
-
-
 class Student
 {
     public $id;
     public $name;
     public $phone;
     public $address;
-
+    
     public function __construct( $name, $phone, $address)
     {
         $this->name = $name;
